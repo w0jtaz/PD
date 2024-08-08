df0a63b16c5aa8f04d8c7d0a79003524db4e69e0
w0jtaz <wojtaz@vp.pl>
2024-08-08 21:32:14 +0200

    Drobne zmiany wizualne

--- app/users/views.py
+++ app/users/views.py
@@ -11,7 +11,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Successfully Created for {username} Login In Now!!!')
+            messages.success(request, f'Konto {username} zostało utworzone pomyślnie, zaloguj się już teraz!')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -33,7 +33,7 @@ def profile_update(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated!')
+            messages.success(request, f'Konto zostało zaktualizowane!')
             return redirect('profile')
 
     else:
